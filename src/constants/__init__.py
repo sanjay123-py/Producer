984f5dc5e5dc618a57d1e24bed12b3e79a89a016
sanjay123-py <sanjaysanjay1270@gmail.com>
2024-02-07 22:34:40 +0530

    cicd-pred

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 SCHEMA_FILE_PATH = '/producer/sample_data/movie_analytics_data/schema_structure.txt'
+# SCHEMA_FILE_PATH = '/Users/sanjay/Desktop/producer/sample_data/movie_analytics_data/schema_structure.txt'
 DATA_PATH = 'movie_analytics_data/ratings.dat'
 BUCKET_NAME = 'rds-movie-analytics'
 REGION = 'ap-south-1'
