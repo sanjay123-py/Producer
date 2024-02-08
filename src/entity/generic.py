d90a6c7e1c46fd9af19fd65048aed7fc148bc15c
sanjay123-py <sanjaysanjay1270@gmail.com>
2024-02-08 13:43:02 +0530

    cicd prep-6

--- src/entity/generic.py
+++ src/entity/generic.py
@@ -18,9 +18,9 @@ class Generic:
     def s3_obj(cls):
         s3 = boto3.resource(
             service_name="s3",
-            region_name=REGION,
-            aws_access_key_id=os.getenv('ACCESS_KEY'),
-            aws_secret_access_key=os.getenv('ACCESS_SECRET_ACCESS_KEY')
+            region_name=os.getenv('AWS_REGION'),
+            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
         )
         s3client = s3.Bucket(BUCKET_NAME).Object(DATA_PATH).get()
         return s3client
